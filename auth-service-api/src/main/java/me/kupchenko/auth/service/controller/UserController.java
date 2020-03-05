5cd10d9f06ccf567a41e7a231946a91871631ded
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2020-03-05 23:14:14 +0200

    Move path var to impl

--- auth-service-api/src/main/java/me/kupchenko/auth/service/controller/UserController.java
+++ auth-service-api/src/main/java/me/kupchenko/auth/service/controller/UserController.java
@@ -2,11 +2,10 @@ package me.kupchenko.auth.service.controller;
 
 import me.kupchenko.auth.service.dto.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/user")
 public interface UserController {
     @GetMapping("/{username}")
-    UserDto findUserByUsername(@PathVariable String username);
+    UserDto findUserByUsername(String username);
 }
--- auth-service-impl/src/main/java/me/kupchenko/auth/service/controller/UserControllerImpl.java
+++ auth-service-impl/src/main/java/me/kupchenko/auth/service/controller/UserControllerImpl.java
@@ -3,12 +3,13 @@ package me.kupchenko.auth.service.controller;
 import lombok.AllArgsConstructor;
 import me.kupchenko.auth.service.dto.UserDto;
 import me.kupchenko.auth.service.service.UserService;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @AllArgsConstructor
 public class UserControllerImpl implements UserController {
     private UserService userService;
 
-    public UserDto findUserByUsername(String username) {
+    public UserDto findUserByUsername(@PathVariable String username) {
         return userService.findUserByUsername(username);
     }
 }
